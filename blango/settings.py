b5a4881ca46d98648b7d3ea465a425e8b1566dc3
Milad Hoseinzadeh <milad.hz75@gmail.com>
2023-07-17 21:04:21 +0000

    Added comment to posts and crispy forms

--- blango/settings.py
+++ blango/settings.py
@@ -45,6 +45,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'crispy_forms',
+    'crispy_bootstrap5',
+
     'blog.apps.BlogConfig',
 ]
 
@@ -132,3 +135,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Crispy forms
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
