936a0a16aa916ca1dc05bba607e2b5c01b99e12b
Milad Hoseinzadeh <milad.hz75@gmail.com>
2023-08-02 22:28:42 +0000

    refactor: added simple logging

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,16 @@
+import logging
+
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 
 from blog.forms import CommentForm
 from blog.models import Post, Comment
 
-# Create your views here.
+logger = logging.getLogger(__name__)
+
 def index(request):
   posts = Post.objects.filter(published_at__lte=timezone.now())
+  logger.debug("Got %d posts", len(posts))
   context = {
     "posts": posts,
   }
@@ -24,6 +28,7 @@ def post_detail(request, slug):
         comment.content_object = post
         comment.creator = request.user
         comment.save()
+        logger.info("Created comment on Post %d for user %s", post.pk, request.user)
         return redirect(request.path_info)
 
     else:
